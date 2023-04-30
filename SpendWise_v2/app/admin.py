4258bcaedbb8ef56de275d3a7572f5b062e02e65
Rajeev Tapadia <rtapadia730@gmail.com>
2023-04-30 22:11:28 +0530

    profile creation implemented

--- SpendWise_v2/app/admin.py
+++ SpendWise_v2/app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from . models import Profile
 
 # Register your models here.
+
+# unregister groups
+admin.site.unregister(Group)
+
+# mix profile info into user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+# extend user model
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline]
+
+# unregister and register user again
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+
+# register profile
+# admin.site.register(Profile)
+
+
